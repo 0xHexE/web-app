6cae12b850adc09422777e5de445520eb29d358f
Omkar Yadav <me@f22.dev>
2022-02-11 23:32:51 +0400

    1. fix where sidebar was not open initial

--- libs/common-ui/src/lib/Component/Sidebar/Component/SidebarContent/index.tsx
+++ libs/common-ui/src/lib/Component/Sidebar/Component/SidebarContent/index.tsx
@@ -6,7 +6,12 @@ export function SidebarContent() {
   const { items } = useSidebarConfig();
 
   return (
-    <Accordion accordion overrides={{ Root: { style: { flexGrow: 1 } } }}>
+    <Accordion
+      accordion
+      overrides={{ Root: { style: { flexGrow: 1 } } }}
+      // TODO: THIS NEEDS TO CHANGE TO ACTIVE ONE
+      initialState={{ expanded: [items?.[0].key] }}
+    >
       {items.map((item) => {
         return (
           <RenderSidebarItem item={item} rootRender={true} key={item.key} />
