856a0373622185fc6363d9d6485db7d921544a56
Omkar Yadav <me@f22.dev>
2022-02-12 01:34:09 +0400

    1. fix lint issue

--- libs/common-ui/src/lib/BaseWeb/Hooks/useUpdateAppTheme.spec.ts
+++ libs/common-ui/src/lib/BaseWeb/Hooks/useUpdateAppTheme.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useUpdateAppTheme } from '@web-app/common-ui';
+import { useUpdateAppTheme } from './useUpdateAppTheme';
 
 describe('useUpdateAppTheme', function () {
   it('should change the langauge', function () {
